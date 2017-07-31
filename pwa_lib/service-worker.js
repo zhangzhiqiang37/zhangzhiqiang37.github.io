fcf3e58001605eda6ef73598ef98920d67bb2230
zhangzhiqiang04 <zhangzhiqiang04@baidu.com>
2017-07-31 10:54:37 +0800

    Site updated: 2017-07-31 10:54:37

--- pwa_lib/service-worker.js
+++ pwa_lib/service-worker.js
@@ -1,14 +1,32 @@
-var cacheKey = 'hexo-pwa-cache' + self.registration ? self.registration.scope : '';
+var cacheKey = 'hexo-pwa-cache-' + self.registration ? self.registration.scope : '';
 
-this.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheKey).then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/css/style.css',
-                '/js/script.js'
-            ]);
-        })
+function getCache(request) {
+    return caches.match(request).then(function (response) {
+        if (response) {
+            return response;
+        }
+    });
+}
+
+self.addEventListener('fetch', function (evt) {
+    var request = evt.request;
+    evt.respondWith(
+        fetch(request)
+            .then(function (response) {
+
+                // 远程数据获取失败
+                if (!response || response.status !== 200) {
+                    return getCache(request);
+                }
+
+                var responseClone = response.clone();
+                caches.open(cacheKey).then(function (cache) {
+                    cache.put(request, responseClone);
+                });
+                return response;
+            })
+            .catch(function (err) {
+                return getCache(request);
+            })
     );
 });
