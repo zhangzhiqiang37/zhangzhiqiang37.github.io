ebb0a323d129d9aa6a09fcd1921b551c1f49d7cd
zhangzhiqiang04 <zhangzhiqiang04@baidu.com>
2017-07-31 11:36:08 +0800

    Site updated: 2017-07-31 11:36:08

--- pwa_lib/service-worker.js
+++ pwa_lib/service-worker.js
@@ -19,7 +19,7 @@ self.addEventListener('install', function (event) {
     event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', function (evnet) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
         Promise.all([
 
